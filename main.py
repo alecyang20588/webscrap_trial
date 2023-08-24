c38c4e271232c72be8cc85ea23cffb65d5e64e60
alecyang20588 <124280571+alecyang20588@users.noreply.github.com>
2023-08-24 21:07:06 +0100

    Update main.py
    worked for retrieve info

--- main.py
+++ main.py
@@ -1,34 +1,30 @@
-import logging
-import logging.handlers
-import os
-
 import requests
+from bs4 import BeautifulSoup
+import re
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+# URL of the webpage
+webpage_url = "https://raw.githubusercontent.com/alecyang20588/webscrap_trial/main/Test_Scanware.html"
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
+# Fetch the content of the webpage
+response = requests.get(webpage_url)
+response.raise_for_status()  # Raise an exception for HTTP errors
 
+# Parse the content with BeautifulSoup
+soup = BeautifulSoup(response.text, 'html.parser')
 
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+# Use regex to find the div containing text like "Number of evaluation cycles..."
+pattern = re.compile(r'Number of evaluation cycles.*', re.IGNORECASE)
+evaluation_cycles_div = soup.find("div", class_="small", text=pattern)
 
-    r = requests.get('Test_Scanware.html')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+# Find the next td containing the number of evaluation cycles
+if evaluation_cycles_div:
+    cycles_td = evaluation_cycles_div.find_next("td", class_="td2")
+    cycles_value = cycles_td.div.text.strip() if cycles_td and cycles_td.div else "Not found"
+else:
+    cycles_value = "Partial match not found"
+
+# Write the extracted number to a log file
+with open("log.txt", "a") as file:  # We use 'a' mode to append to the file
+    file.write(f"Number of Evaluation Cycles: {cycles_value}\n")
+
+print("Data appended to log.txt")
